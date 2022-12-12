e1597920810b382d82d182ffbe79351df5b0436d
DylanRhinehart <dylanrhinehart@gmail.com>
2022-12-12 15:35:26 -0800

    commit tree 438319b0d90e38514e4a3b61d41403b9ac94dfdb

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    let newArray = [];
+        newArray.push(array[0]);
+        newArray.push(array[1]);
+    return newArray;
+}
+
+const returnLastTwoDrivers = (array) => {
+    let newArray = [];
+        newArray.push(array[array.length - 2]);
+        newArray.push(array[array.length - 1]);
+    return newArray;
+}
+
+var selectingDrivers = [returnFirstTwoDrivers ,returnLastTwoDrivers];
+
+function createFareMultiplier(multi) {
+    const fareMultiplier = (fare) => fare * multi;
+
+    return fareMultiplier;
+}
+
+
+function fareDoubler(fare) {
+    const double = createFareMultiplier(2)(fare); 
+    return double;
+}
+
+function fareTripler(fare) {
+    const triple = createFareMultiplier(3)(fare);
+    return triple;
+}
+
+function selectDifferentDrivers(array, x) {
+   const drivers = x(array);
+   return drivers;
+}
